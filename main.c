24629d470d2a40dd3f2cae249babb3b6051595a4
sopiU6 15 <nomisp96@hotmail.fr>
2017-07-20 16:15:33 +0200

    setting up

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#define MAX 64
 
 int main(int argc, char *argv[]) {
-
 
     return 1;
 }
---
+++ message.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char *fill_the_message(char *input) {
+    static char message[64];
+
+    return message;
+}
+
