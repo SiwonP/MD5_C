2392966add9960ff12aed2b4c57330b91fc4db6c
sopiU6 15 <nomisp96@hotmail.fr>
2017-07-24 15:29:11 +0200

    protection against no argument given

--- main.c
+++ main.c
@@ -6,6 +6,11 @@
 #define MAX 64
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("usage : %s <string>\n", argv[0]);
+        return 1;
+    }
+
     unsigned long *message;
 
     message = complete(argv[1]);
